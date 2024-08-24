6e6e7c8f37103954b3a9992aca902665828ac3d2
Ziqiang <132073627+wzq0802@users.noreply.github.com>
2024-08-24 18:00:40 +0800

    Add files via upload

--- net.py
+++ net.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from functools import partial
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import mobilenet_v2
 class Conv_Block2(nn.Module):
     def __init__(self, in_channel, out_channel):
         super(Conv_Block2, self).__init__()
@@ -120,6 +124,8 @@ class UNet(nn.Module):
         O4 = self.c9(self.u4(O3, R1))
 
         return self.out(O4)
+
+
 class edge(nn.Module):
     def __init__(self):
         super(edge, self).__init__()
@@ -130,7 +136,7 @@ class edge(nn.Module):
             nn.Conv2d(1, 1, kernel_size=1),
             nn.BatchNorm2d(1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(1, 1, kernel_size=3, padding=1),
+            nn.Conv2d(1, 1, kernel_size=3, padding=1,dilation=1),
             nn.BatchNorm2d(1),
             nn.ReLU(inplace=True),
             nn.Conv2d(1, 1, kernel_size=1),
@@ -138,10 +144,10 @@ class edge(nn.Module):
         )
 
         self.residual_connection = nn.Sequential(
-            nn.Conv2d(1, 1, kernel_size=3, padding=1),
+            nn.Conv2d(1, 1, kernel_size=3, padding=1,dilation=1),
             nn.BatchNorm2d(1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(1, 1, kernel_size=3, padding=1),
+            nn.Conv2d(1, 1, kernel_size=3, padding=1,dilation=1),
             nn.BatchNorm2d(1)
         )
 
@@ -160,6 +166,7 @@ class edge(nn.Module):
 
         edge_features = x_avg_pool + x_max_pool
         return edge_features
+
 
 class DualSegNet(nn.Module):
     def __init__(self, n=8,edge_model_path='edge.pth'):
@@ -499,7 +506,6 @@ class RB2d(torch.nn.Module):
         return torch.nn.functional.relu(y + x)
 
 
-
 class ResUNet(nn.Module):
     def __init__(self,n=8):
         super(ResUNet, self).__init__()
@@ -605,3 +611,171 @@ class DualResUNet(nn.Module):
         O4 = self.c9(self.u4(O3, x_256))
         return self.out(O4)
 
+
+def weights_init(net, init_type='normal', init_gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and classname.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                torch.nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, downsample_factor=8, pretrained=True):
+        super(MobileNetV2, self).__init__()
+
+        # 加载预训练的 MobileNetV2 模型
+        model = mobilenet_v2(pretrained=pretrained)
+
+        # 修改第一层卷积以支持单通道输入
+        model.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
+
+        self.features = model.features[:-1]
+        self.total_idx = len(self.features)
+        self.down_idx = [2, 4, 7, 14]
+
+        if downsample_factor == 8:
+            for i in range(self.down_idx[-2], self.down_idx[-1]):
+                self.features[i].apply(partial(self._nostride_dilate, dilate=2))
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(partial(self._nostride_dilate, dilate=4))
+        elif downsample_factor == 16:
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(partial(self._nostride_dilate, dilate=2))
+        else:
+            raise ValueError('downsample_factor is {}, but must be one of 8 and 16'.format(downsample_factor))
+
+    def _nostride_dilate(self, m, dilate):
+        classname = m.__class__.__name__
+        if classname.find('Conv2dNormActivation') != -1:
+            if m[0].stride == (2, 2):
+                m[0].stride = (1, 1)
+                if m[0].kernel_size == (3, 3):
+                    m[0].dilation = (dilate // 2, dilate // 2)
+                    m[0].padding = (dilate // 2, dilate // 2)
+            else:
+                if m[0].kernel_size == (3, 3):
+                    m[0].dilation = (dilate, dilate)
+                    m[0].padding = (dilate, dilate)
+
+    def forward(self, x):
+        low_level_features = self.features[:4](x)
+        x = self.features[4:](low_level_features)
+        return low_level_features, x
+
+
+class ASPP(nn.Module):
+    def __init__(self, in_channels, out_channels, rate=1, bn_momentum=0.1):
+        super(ASPP, self).__init__()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=1, padding=0, dilation=rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=6 * rate, dilation=6 * rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch3 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=12 * rate, dilation=12 * rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch4 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=18 * rate, dilation=18 * rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch5_conv = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=1, padding=0, bias=False)
+        self.branch5_bn = nn.BatchNorm2d(out_channels, momentum=bn_momentum)
+        self.branch5_relu = nn.ReLU(inplace=True)
+
+        self.conv_cat = nn.Sequential(
+            nn.Conv2d(out_channels * 5, out_channels, kernel_size=(1, 1), stride=1, padding=0),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        conv1x1 = self.branch1(x)
+        conv3x3_1 = self.branch2(x)
+        conv3x3_2 = self.branch3(x)
+        conv3x3_3 = self.branch4(x)
+
+        global_feature = torch.mean(x, dim=2, keepdim=True)
+        global_feature = torch.mean(global_feature, dim=3, keepdim=True)
+        global_feature = self.branch5_conv(global_feature)
+        global_feature = self.branch5_bn(global_feature)
+        global_feature = self.branch5_relu(global_feature)
+        global_feature = F.interpolate(global_feature, size=(h, w), mode='bilinear', align_corners=True)
+
+        feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
+        result = self.conv_cat(feature_cat)
+        return result
+
+
+class DeepLabV3plus(nn.Module):
+    def __init__(self, backbone='mobilenet', pretrained=True, downsample_factor=16):
+        super(DeepLabV3plus, self).__init__()
+        if backbone == 'mobilenet':
+            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 320
+            low_level_channels = 24
+        else:
+            raise ValueError('Unsupported backbone - {}, Use mobilenet'.format(backbone))
+
+        self.aspp = ASPP(
+            in_channels=in_channels,
+            out_channels=256,
+            rate=16 // downsample_factor
+        )
+
+        self.shortcut_conv = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, kernel_size=(1, 1)),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )
+
+        self.cat_conv = nn.Sequential(
+            nn.Conv2d(48 + 256, 256, kernel_size=(3, 3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1)
+        )
+        self.cls_conv = nn.Conv2d(256, 1, kernel_size=(1, 1))  # 修改输出通道数为1
+
+    def forward(self, x):
+        H, W = x.size(2), x.size(3)
+
+        low_level_feature, x = self.backbone(x)
+        x = self.aspp(x)
+        low_level_feature = self.shortcut_conv(low_level_feature)
+
+        x = F.interpolate(x, size=(low_level_feature.size(2), low_level_feature.size(3)), mode='bilinear',
+                          align_corners=True)
+        x = self.cat_conv(torch.cat((x, low_level_feature), dim=1))
+        x = self.cls_conv(x)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+        return x
